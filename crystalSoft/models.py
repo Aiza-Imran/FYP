7d9e70fea473b11898dd443d0135269fbcaeca16
Aiza Imran <i.ayza3073@gmail.com>
2024-02-10 16:16:34 +0500

    FYP-Modified user table and its permissions

--- crystalSoft/models.py
+++ crystalSoft/models.py
@@ -1,6 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=30)
+class Permissions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    add_expense = models.BooleanField()
+    add_edit_income = models.BooleanField()
+    add_new_item = models.BooleanField()
+    add_purchase = models.BooleanField()
+    view_dashboard = models.BooleanField()
+    view_sale_invoices = models.BooleanField()
+    view_reports = models.BooleanField()
+    view_stock = models.BooleanField()
+    view_bill_profit = models.BooleanField()
+    open_new_day = models.BooleanField()
+    close_day = models.BooleanField()
+    attendence_of_employee = models.BooleanField()
+    day_summary = models.BooleanField()
+    change_sale_price = models.BooleanField()
+    change_price = models.BooleanField()
+    edit_invoices = models.BooleanField()
+    discount_in_sale_price = models.BooleanField()
+    make_receipt_voucher = models.BooleanField()
     
+    def __str__(self):
+        return  f"{self.user} Permission Set"
+    
+        
